26e5a2999471874772139326e54ce991242841e3
Sergio Díaz <codediaz@outlook.es>
2025-01-19 01:16:23 -0500

    feat: Contact info added

--- constants/const.js
+++ constants/const.js
@@ -6,3 +6,9 @@ export const ABOUT_ME = {
     title: "Software Engineer",
     description: "I'm a passionate learner who's always willing to learn and work across technologies and domains. I love to explore new technologies and leverage them to solve real-life problems.",
 }
+
+export const CONTACT = {
+    email: "codediaz@outlook.es",	
+    phone: "+34 666 666 666",   
+    address: "Guayaquil, Ecuador"
+}
