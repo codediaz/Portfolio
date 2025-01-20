ab0c0caa8917050ef1889461b051d436fedc86c5
Sergio Díaz <codediaz@outlook.es>
2025-01-19 19:49:14 -0500

    feat: New portfolio desing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 const App =() => {
-  return <div className="bg-blue-500 text-white p-8">Hello Portfolio, Tailwindd CSS!</div>
-}
 
-export default App
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-full w-full"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+      </div>
+    </div>
+  );
+};
+
+export default App;
