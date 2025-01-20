cbb8cca7bfeff0d711ba8c897165830287e2ea5a
Sergio Díaz <codediaz@outlook.es>
2025-01-20 00:19:57 -0500

    feat: Technologies section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Technologies from "./components/Technologies";
 const App =() => {
 
   return (
@@ -12,6 +13,7 @@ const App =() => {
         <Navbar />
         <Hero />
         <About/>
+        <Technologies/>
       </div>
     </div>
   );
