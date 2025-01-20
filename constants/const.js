923fd392ecb6c69d11166b77e30611d4af6e0d0c
Sergio Díaz <codediaz@outlook.es>
2025-01-19 19:47:58 -0500

    feat: Update consts

--- constants/const.js
+++ constants/const.js
@@ -2,8 +2,8 @@
 
 export const ABOUT_ME = {
     fullName: "Sergio Díaz",
-    nickname: "codediaz",
-    title: "Software Engineer",
+    nickname: "Codediaz",
+    title: "Lead Software Engineer",
     description: "I'm a passionate learner who's always willing to learn and work across technologies and domains. I love to explore new technologies and leverage them to solve real-life problems.",
 }
 
