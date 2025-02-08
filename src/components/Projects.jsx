a6ed30930b7ef27200f2f337abc8ab36639a00a5
Sergio Díaz <codediaz@outlook.es>
2025-02-08 00:37:53 -0500

    feat: Project section animated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,17 @@
 import { PROJECTS } from "../../constants/const"
+import { motion } from "framer-motion";
 
 const Projects = () => {
     return(
         <div className="border-b border-neutral-900 pb-4">
-            <h2 className="my-20 text-center text-4xl">
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-20 text-center text-4xl"
+            >
                 Projects
-            </h2>
+            </motion.h2>
             <div>
                 {PROJECTS.map((project, index) => (
                     <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
