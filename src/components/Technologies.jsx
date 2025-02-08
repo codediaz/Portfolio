28fec126c78dd2f567e337fa44e78f49c6138b0a
Sergio Díaz <codediaz@outlook.es>
2025-02-07 21:18:47 -0500

    feat:Added links to technologies logo

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,34 +2,64 @@ import { RiReactjsLine } from "react-icons/ri";
 import { FaPython, FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql, BiLogoDjango } from "react-icons/bi";
 import { SiPhp, SiMysql } from "react-icons/si";
+
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
       <h2 className="my-20 text-center text-4xl">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <a
+          href="https://react.dev/"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-5xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </a>
+        <a
+          href="https://nodejs.org/en"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaNodeJs className="text-5xl text-green-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </a>
+        <a
+          href="https://www.python.org/"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaPython className="text-5xl text-violet-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </a>
+        <a
+          href="https://www.djangoproject.com/"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <BiLogoDjango className="text-5xl text-amber-800" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </a>
+        <a
+          href="https://www.php.net/"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiPhp className="text-5xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </a>
+        <a
+          href="https://www.postgresql.org/"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <BiLogoPostgresql className="text-5xl text-sky-700" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </a>
+        <a
+          href="https://www.mysql.com/"
+          target="_blank"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMysql className="text-5xl text-cyan-400" />
-        </div>
+        </a>
       </div>
     </div>
   );
 };
+
 export default Technologies;
