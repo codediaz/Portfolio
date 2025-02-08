502efa792c7699d5e4c0f1307260b3540d25deb8
Sergio Díaz <codediaz@outlook.es>
2025-02-07 22:31:10 -0500

    feat: Added animation for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import AboutImage from "../assets/aboutme.jpg"
+import {motion} from 'framer-motion';
 const About = () => {
 
     return (
@@ -8,7 +9,12 @@ const About = () => {
                     <span className="text-neutral-500"> Me</span>
             </h2>
             <div className="flex flex-wrap items-center">
-                <div className="w-full lg:w-1/2 lg:p-8">
+            <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
+            className="w-full lg:w-1/2 lg:p-8"
+            >
                 <div className="flex items-center justify-center">
                     <img
                     className="w-[70%]  rounded-2xl"
@@ -16,14 +22,18 @@ const About = () => {
                     alt="Gem Coding"
                     />
                 </div>
-                </div>
-                <div className="w-full lg:w-1/2">
+            </motion.div>
+            <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 0.5 }}
+                className="w-full lg:w-1/2"
+            >
                 <div className="flex justify-center lg:justify-start">
                     <p className="my-2 max-w-xl py-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, eum esse? Quia quisquam ea magni reiciendis, recusandae in, vel a libero deserunt tempora tenetur! Nisi possimus ab dolor harum esse suscipit exercitationem omnis praesentium odio earum odit iusto debitis laudantium quam error, minus atque pariatur? Porro dolorem amet atque dicta esse inventore eius alias asperiores expedita accusantium. Ipsum quo nemo eos ex odit praesentium laboriosam fugit molestiae adipisci minima natus, soluta omnis quos doloremque! Cum velit quibusdam doloremque quisquam numquam debitis ad! Minus, commodi modi non quam, eaque, unde earum adipisci voluptas enim nisi ex eos maiores vitae ab eum?.</p>
                 </div>
-                </div>
-            </div>  
-               
+            </motion.div>
+            </div>   
         </div>
     );
 
