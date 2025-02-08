f55d4b5e0fdadfabef51cbdc74c4eb6ab8412589
Sergio Díaz <codediaz@outlook.es>
2025-02-08 00:33:16 -0500

    feat: Animation for contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,47 @@
 import { CONTACT } from "../../constants/const";
+import { motion } from "framer-motion";
 
 const Contact = () => {
     return(
         <div className="border-b border-neutral-800 pb-4">
-            <h2 className="my-10 text-center text-4xl">
+        <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+        >
             Get in Touch
-            </h2>
-            <div className="flex flex-col items-center">
-                <div className="flex flex-col items-center text-center tracking-tighter">
-                    <p className="text-lg text-neutral-400 my-4">
-                        Email: {CONTACT.email}
-                    </p>
-                    <p className="text-lg text-neutral-400">
-                        Phone: {CONTACT.phone}
-                    </p>
-                    <p className="text-lg text-neutral-400">
-                        Address: {CONTACT.address}
-                    </p>
-                </div>
+        </motion.h2>
+        <div className="flex flex-col items-center tracking-tighter">
+            <div className="flex flex-col items-center text-center">
+                <motion.p
+                 whileInView={{ opacity: 1, x: 0 }}
+                 initial={{ opacity: 0, x: -100 }}
+                 transition={{ duration: 1 }}
+                 className="text-lg text-neutral-400 my-4"
+
+                >
+                    Email: {CONTACT.email}
+                </motion.p>
+                <motion.p
+                 whileInView={{ opacity: 1, x: 0 }}
+                 initial={{ opacity: 0, x: -100 }}
+                 transition={{ duration: 1 }}
+                 className="text-lg text-neutral-400"
+                >
+                   Phone: {CONTACT.phone}
+                </motion.p>
+                <motion.p
+                 whileInView={{ opacity: 1, x: 0 }}
+                 initial={{ opacity: 0, x: -100 }}
+                 transition={{ duration: 1 }}
+                 className="text-lg text-neutral-400"
+                >
+                    Address: {CONTACT.address}
+                </motion.p>
+                
             </div>
+        </div>
         </div>
     );
 };
