e207edec4bc34dc4d42cb8f2faf74ba27ea25166
Sergio Díaz <codediaz@outlook.es>
2025-02-07 21:28:38 -0500

    feat: Added Experience section

--- constants/const.js
+++ constants/const.js
@@ -12,3 +12,20 @@ export const CONTACT = {
     phone: "+34 666 666 666",   
     address: "Guayaquil, Ecuador"
 }
+
+export const EXPERIENCES = [
+    {
+        year: "2023 - Present",
+        role: "Senior Full Stack Developer",
+        company: "Google Inc.",
+        description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+        technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    },
+    {
+        year: "2022 - 2023",
+        role: "Frontend Developer",
+        company: "Adobe",
+        description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+        technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    },
+];
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
 const App =() => {
 
   return (
@@ -14,6 +15,7 @@ const App =() => {
         <Hero />
         <About/>
         <Technologies/>
+        <Experience/>
       </div>
     </div>
   );
